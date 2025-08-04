97f1e953162b0d8656341124f4ff9a5987ac00cb
celizondo <calebelizondo@tamu.edu>
2025-08-04 15:07:08 -0500

    finished project 5

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by user276992 on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
